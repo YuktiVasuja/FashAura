ba571611f2c1f3a73526fea6fa3a5994c8dd78ef
YuktiVasuja <yuktivasuja0408@gmail.com>
2025-12-25 22:48:10 +0530

    Remove Firebase API keys from firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBJo0xSuqJOXWpB5RGqwhqBW3qU3Gse5oA",
-  authDomain: "fashaura-cdf80.firebaseapp.com",
-  projectId: "fashaura-cdf80",
-  storageBucket: "fashaura-cdf80.firebasestorage.app",
-  messagingSenderId: "621820045493",
-  appId: "1:621820045493:web:af59bcd12aca338249f6fc",
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_BUCKET",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
